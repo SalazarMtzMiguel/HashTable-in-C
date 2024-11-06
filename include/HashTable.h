3f98c7faa11befef5cce1e4157b6986ebe412445
Miguel Angel Salazar Martinez <128423820+SalazarMtzMiguel@users.noreply.github.com>
2024-11-06 16:00:43 -0600

    primer commit

---
+++ include/HashTable.h
@@ -0,0 +1,12 @@
+//
+// Created by Mikec on 06/11/2024.
+//
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+typedef struct Node {
+    int key;
+    char *value;
+    struct Node *next;
+}Node;
+#endif //HASHTABLE_H
---
+++ src/HashTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mikec on 06/11/2024.
+//
+
+#include "HashTable.h"
